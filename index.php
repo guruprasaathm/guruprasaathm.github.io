f58eccf4a0cd205ebc833a42513fda5b5bcca5d1
GuruPrasaathM <56358942+GuruPrasaathM@users.noreply.github.com>
2024-05-06 15:07:21 +0530

    php does not reflect

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-   $imagesDir = 'cats/';
-   $images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-   $randomImage = $images[array_rand($images)];
-   echo "<img src='$randomImage'>";
-?>
